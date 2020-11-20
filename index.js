8eb10b6264036789b1528329d08ed387bc8d20de
LMSvishal <vishal.purohit@lmsin.com>
2020-11-20 13:43:56 +0530

    port changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const port = 4000;
+const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 mongoose.pluralize(null);
 
